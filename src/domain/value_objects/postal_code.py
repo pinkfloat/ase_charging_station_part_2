657dafba10b2c5b79c0239be6735c769e3b5228f
Sabrina Otto <otto.sabrina@tuta.com>
2025-01-26 18:22:38 +0100

    pass tests: update postal code init function

--- src/domain/value_objects/postal_code.py
+++ src/domain/value_objects/postal_code.py
@@ -1,4 +1,7 @@
 # src/domain/value_objects/postal_code.py
 class PostalCode:
     def __init__(self, plz):
+        plz = str(plz)
+        if len(plz) != 5 or not plz.isdigit():
+            raise ValueError("Invalid postal code")
         self.plz = plz
