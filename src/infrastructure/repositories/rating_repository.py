ac1cec94704d53af86876834226d330092cdc235
Sabrina Otto <otto.sabrina@tuta.com>
2025-01-29 09:48:59 +0100

    copy RatingRepository out of ChargingStationRepository

---
+++ src/infrastructure/repositories/rating_repository.py
@@ -0,0 +1,59 @@
+# src/infrastructure/repositories/rating_repository.py
+from datetime import datetime
+from firebase_admin import credentials, initialize_app, db
+from domain.entities.rating import Rating
+
+class RatingRepository:
+    def __init__(self, firebase_secret_json):
+        # Initialize Firebase only once
+        if not db._apps:  # Check if Firebase is already initialized
+            cred = credentials.Certificate(firebase_secret_json)
+            initialize_app(cred, {
+                'databaseURL': 'https://ase-charging-default-rtdb.europe-west1.firebasedatabase.app/'
+            })
+        
+        self.station_ratings_ref = db.reference("ratings")
+        self.station_ratings = []
+    
+    def load_station_ratings_from_database(self):
+        rating_dict = self.station_ratings_ref.get() or {}  # Get all ratings or an empty dictionary
+
+        for rating_id, data in rating_dict.items():
+            rating = Rating(
+                user_id=data["user_id"],
+                station_id=data["charging_station_id"],
+                date=data["review_date"],
+                value=data["review_star"],
+                comment=data["review_text"],
+            )
+            self.station_ratings.append(rating)
+        return self.station_ratings
+
+    def create_rating(self, user_id, station_id, value, comment):
+        """Generates a new rating object."""
+        rating = Rating(
+            user_id=user_id,
+            station_id=station_id,
+            date=datetime.now().isoformat(),
+            value=value,
+            comment=comment
+        )
+        return rating
+    
+    def save_rating_to_repo(self, rating):
+        """Appends the rating on the repository rating list."""
+        if not isinstance(rating, Rating):
+            raise ValueError("Invalid rating object")
+        self.station_ratings.append(rating)
+
+    def save_rating_to_database(self, rating):
+        """Saves a new rating to the database."""
+        if not isinstance(rating, Rating):
+            raise ValueError("Invalid rating object")
+        self.station_ratings_ref.push({
+            "user_id": rating.user_id,
+            "charging_station_id": rating.station_id,
+            "review_star": rating.value,
+            "review_text": rating.comment,
+            "review_date": rating.date
+        })
